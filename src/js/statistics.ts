dc523de1295249578dc62a0abe574152bc549646
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-11-29 10:15:32 +0100

    Updated table to work woth bootstrap

--- src/js/statistics.ts
+++ src/js/statistics.ts
@@ -81,17 +81,22 @@ export class Statistics
             //table
             //table create
             let table: HTMLTableElement = document.createElement("table");
-            table.setAttribute("id", "table" + i.toString()); // !!
+            table.setAttribute("class", "table table-striped");
 
             //table collums
+            let tabaleThead: HTMLTableSectionElement = document.createElement("thead"); // thead requered for bootstrap
+
             let tablecolumnRow: HTMLTableRowElement = document.createElement("tr"); //row til columns
 
             let tablecolumn0: HTMLTableHeaderCellElement = document.createElement("th") // paragraph column
-            // tablecolumn0.innerHTML = "test"
+            tablecolumn0.innerHTML = "Titler"
             
             let tablecolumn1: HTMLTableHeaderCellElement = document.createElement("th") // input column
-            // tablecolumn1.innerHTML = "test 2"
+            tablecolumn1.innerHTML = "Input"
 
+
+            // table tbody (used by bootstrap)
+            let tabletbody: HTMLTableSectionElement = document.createElement("tbody");
 
             // table row
             let tableRow0 : HTMLTableRowElement = document.createElement("tr");
@@ -109,6 +114,9 @@ export class Statistics
             tableRowName.setAttribute("id", "rowName" + i.toString()); // !!
             tableRowName.innerHTML = "Sensor navn: "
 
+            let tableRowNameTd: HTMLTableDataCellElement = document.createElement("td");
+
+            // input name
             let tableRowNameInput : HTMLInputElement = document.createElement("input");
             tableRowNameInput.setAttribute("id", "rowNameInput");
             tableRowNameInput.setAttribute("placeholder", "indtast navn her");
@@ -118,6 +126,9 @@ export class Statistics
             tableRowLower.setAttribute("id", "rowLower" + i.toString()); // !!
             tableRowLower.innerHTML = "laveste grænse: "
 
+            let tableRowLowerTd: HTMLTableDataCellElement = document.createElement("td");
+
+            // input lower
             let tableRowLowerInput : HTMLInputElement = document.createElement("input");
             tableRowLowerInput.setAttribute("id", "rowLowerInput");
             tableRowLowerInput.setAttribute("placeholder", "indtast laveste grænse her");
@@ -127,6 +138,9 @@ export class Statistics
             tableRowUpper.setAttribute("id", "rowUpper" + i.toString()); // !!
             tableRowUpper.innerHTML = "normal grænse: "
 
+            let tableRowUpperTd: HTMLTableDataCellElement = document.createElement("td");
+
+            // input Upper
             let tableRowUpperInput : HTMLInputElement = document.createElement("input");
             tableRowUpperInput.setAttribute("id", "rowUpperInput");
             tableRowUpperInput.setAttribute("placeholder", "indtast normal grænse her");
@@ -144,25 +158,30 @@ export class Statistics
             // table append
             collapse.appendChild(table);
 
-            table.appendChild(tablecolumnRow);
+            table.appendChild(tabaleThead);
+            tabaleThead.appendChild(tablecolumnRow);
             tablecolumnRow.appendChild(tablecolumn0);
             tablecolumnRow.appendChild(tablecolumn1);
 
+            table.appendChild(tabletbody);
 
-            table.appendChild(tableRow0);
+            tabletbody.appendChild(tableRow0);
 
             tableRow0.appendChild(tableRowName);
-            tableRow0.appendChild(tableRowNameInput);
+            tableRow0.appendChild(tableRowNameTd);
+            tableRowNameTd.appendChild(tableRowNameInput);
 
-            table.appendChild(tableRow1);
+            tabletbody.appendChild(tableRow1);
 
             tableRow1.appendChild(tableRowLower);
-            tableRow1.appendChild(tableRowLowerInput);
+            tableRow1.appendChild(tableRowLowerTd);
+            tableRowLowerTd.appendChild(tableRowLowerInput);
 
-            table.appendChild(tableRow2);
+            tabletbody.appendChild(tableRow2);
 
             tableRow2.appendChild(tableRowUpper);
-            tableRow2.appendChild(tableRowUpperInput)
+            tableRow2.appendChild(tableRowUpperTd);
+            tableRowUpperTd.appendChild(tableRowUpperInput)
 
             // button text
             btn.innerText = "#" + (i + 1).toString() + " Sensor";
