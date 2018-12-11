0557ae8d3a5190bbc1f38c45460cabad5b037053
Michael-MO <Michael-MO@users.noreply.github.com>
2018-12-11 11:20:30 +0100

    Prepare for API call

--- dist/bundle.js
+++ dist/bundle.js
@@ -2044,6 +2044,44 @@ var Alert = /** @class */ (function () {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Consumption", function() { return Consumption; });
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+
 var Consumption = /** @class */ (function () {
     function Consumption() {
         this.WaterAmount = 0.5;
@@ -2054,6 +2092,7 @@ var Consumption = /** @class */ (function () {
         this.WateringNumsCell = document.getElementById("consumptionWateringNums");
         this.WaterPriceCell = document.getElementById("consumptionWaterPrice");
         this.TotalCell = document.getElementById("consumptionTotal");
+        this.BASEURI = "https://watermasterapi.azurewebsites.net/api/user/";
         this.calcTotal();
     }
     Consumption.prototype.numFormat = function (num) {
@@ -2063,16 +2102,34 @@ var Consumption = /** @class */ (function () {
             .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.') // Then put a point for every thousand in the number.
         );
     };
-    Consumption.prototype.doConsumption = function () {
-        this.WaterAmountCell.innerText = this.numFormat(this.WaterAmount);
-        this.WateringNumsCell.innerText = this.numFormat(this.WateringNums);
-        this.WaterPriceCell.innerText = this.numFormat(this.WaterPrice);
-        this.TotalCell.innerText = this.numFormat(this.Total);
-    };
     Consumption.prototype.calcTotal = function () {
         var total = 0;
         total = (this.WaterAmount * this.WaterPrice) * this.WateringNums;
         this.Total = total;
+    };
+    Consumption.prototype.doConsumption = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var numberOfWaterings;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        numberOfWaterings = 0;
+                        return [4 /*yield*/, axios__WEBPACK_IMPORTED_MODULE_0___default.a.get(this.BASEURI)
+                                .then(function (response) {
+                                numberOfWaterings = response.data.waterCount;
+                            })];
+                    case 1:
+                        _a.sent();
+                        this.WateringNums = numberOfWaterings;
+                        this.calcTotal();
+                        this.WaterAmountCell.innerText = this.numFormat(this.WaterAmount);
+                        this.WateringNumsCell.innerText = this.numFormat(this.WateringNums);
+                        this.WaterPriceCell.innerText = this.numFormat(this.WaterPrice);
+                        this.TotalCell.innerText = this.numFormat(this.Total);
+                        return [2 /*return*/];
+                }
+            });
+        });
     };
     return Consumption;
 }());
--- src/js/consumption.ts
+++ src/js/consumption.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export class Consumption
 {
     private WaterAmount: number = 0.5;
@@ -9,6 +11,8 @@ export class Consumption
     private WateringNumsCell = document.getElementById("consumptionWateringNums") as HTMLTableCellElement;
     private WaterPriceCell = document.getElementById("consumptionWaterPrice") as HTMLTableCellElement;
     private TotalCell = document.getElementById("consumptionTotal") as HTMLTableCellElement;
+
+    private BASEURI: string = "https://watermasterapi.azurewebsites.net/api/user/";
 
     constructor()
     {
@@ -24,18 +28,29 @@ export class Consumption
         );
     }
 
-    doConsumption(): void
-    {
-        this.WaterAmountCell.innerText = this.numFormat(this.WaterAmount);
-        this.WateringNumsCell.innerText = this.numFormat(this.WateringNums);
-        this.WaterPriceCell.innerText = this.numFormat(this.WaterPrice);
-        this.TotalCell.innerText = this.numFormat(this.Total);
-    }
-
     calcTotal(): void
     {
         let total: number = 0;
         total = (this.WaterAmount * this.WaterPrice) * this.WateringNums;
         this.Total = total;
+    }
+
+    async doConsumption()
+    {
+        let numberOfWaterings: number = 0;
+
+        await axios.get(this.BASEURI)
+        .then(function(response)
+        {
+            numberOfWaterings = response.data.waterCount;
+        });
+
+        this.WateringNums = numberOfWaterings;
+        this.calcTotal();
+
+        this.WaterAmountCell.innerText = this.numFormat(this.WaterAmount);
+        this.WateringNumsCell.innerText = this.numFormat(this.WateringNums);
+        this.WaterPriceCell.innerText = this.numFormat(this.WaterPrice);
+        this.TotalCell.innerText = this.numFormat(this.Total);
     }
 }
